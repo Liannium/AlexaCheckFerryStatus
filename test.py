4c5503c3de2a8c94674787982a70c0c6e1697989
Liannium <meliaerinelizabeth@gmail.com>
2019-12-27 14:45:00 -0800

    Accesses terminals.ashx

--- test.py
+++ test.py
@@ -2,18 +2,17 @@ import requests
 import json
 import vesselfunctions
 
-#terminalurl = "https://www.wsdot.wa.gov/ferries/vesselwatch/Terminals.ashx"
+terminalurl = "https://www.wsdot.wa.gov/ferries/vesselwatch/Terminals.ashx"
 vesselurl = "https://www.wsdot.com/ferries/vesselwatch/Vessels.ashx"
 
-#TODO: Make sure it can handle multiple ferries going to Bainbridge/Seattle at once
-
 vesselresp = requests.get(vesselurl)
-#terminalresp = requests.get(terminalurl)
+terminalresp = requests.get(terminalurl)
 
-if vesselresp.status_code == 200:
+if vesselresp.status_code == 200 and terminalresp.status_code == 200:
     print("Successfully accessed pages!")
-    vessel = json.loads(vesselresp.text)
-    vessellist = vessel["vessellist"]
+    vessels = json.loads(vesselresp.text)
+    vessellist = vessels["vessellist"]
+
     leavingBainbridge = vesselfunctions.findvessel(vessellist, "Bainbridge Island", "SEA-BI")
     if leavingBainbridge:
         for i in range(0, len(leavingBainbridge)):
