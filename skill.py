d11858ded76878611e5702e056ccfc1db12e93ac
Liannium <meliaerinelizabeth@gmail.com>
2019-12-31 18:41:18 -0800

    Added actual code to skill

--- skill.py
+++ skill.py
@@ -6,6 +6,11 @@ from ask_sdk_model import Response
 from ask_sdk_model.ui import SimpleCard
 from ask_sdk_core.dispatch_components import AbstractExceptionHandler
 
+import requests
+import json
+import re
+from vesselfunctions import getbiseattleferries
+
 
 class LaunchRequestHandler(AbstractRequestHandler):
     def can_handle(self, handler_input):
@@ -14,24 +19,42 @@ class LaunchRequestHandler(AbstractRequestHandler):
 
     def handle(self, handler_input):
         # type: (HandlerInput) -> Response
-        speech_text = "Welcome to the Alexa Skills Kit, you can say hello!"
+        response = "I can check the status of the ferry."
 
-        handler_input.response_builder.speak(speech_text).set_card(
-            SimpleCard("Hello World", speech_text)).set_should_end_session(False)
+        handler_input.response_builder.speak(response).set_card(
+            SimpleCard("Check Ferry", response)).set_should_end_session(False)
         return handler_input.response_builder.response
 
 
-class HelloWorldIntentHandler(AbstractRequestHandler):
+class CheckFerryIntentHandler(AbstractRequestHandler):
     def can_handle(self, handler_input):
         # type: (HandlerInput) -> bool
-        return is_intent_name("HelloWorldIntent")(handler_input)
+        return is_intent_name("CheckFerryIntent")(handler_input)
 
     def handle(self, handler_input):
         # type: (HandlerInput) -> Response
-        speech_text = "Hello World, I'm Alexa!"
+        response = ""
+        terminalurl = "https://www.wsdot.wa.gov/ferries/vesselwatch/Terminals.ashx"
+        vesselurl = "https://www.wsdot.com/ferries/vesselwatch/Vessels.ashx"
 
-        handler_input.response_builder.speak(speech_text).set_card(
-            SimpleCard("Hello World", speech_text)).set_should_end_session(
+        vesselresp = requests.get(vesselurl)
+        terminalresp = requests.get(terminalurl)
+
+        if vesselresp.status_code == 200 and terminalresp.status_code == 200:
+            vessels = json.loads(vesselresp.text)
+            vessellist = vessels["vessellist"]
+
+            regex = r'new Date\(\d*\)'
+            terminalstring = re.sub(regex, "null", terminalresp.text, flags=re.MULTILINE)
+            terminals = json.loads(terminalstring)
+            terminallist = terminals["FeedContentList"]
+
+            response = getbiseattleferries(vessellist, terminallist)
+        else:
+            response = "The page could not be successfully accessed"
+
+        handler_input.response_builder.speak(response).set_card(
+            SimpleCard("Check Ferry", response)).set_should_end_session(
             True)
         return handler_input.response_builder.response
 
@@ -55,10 +78,10 @@ class HelpIntentHandler(AbstractRequestHandler):
 
     def handle(self, handler_input):
         # type: (HandlerInput) -> Response
-        speech_text = "You can say hello to me!"
+        response = "I can check the status of the ferry"
 
-        handler_input.response_builder.speak(speech_text).ask(speech_text).set_card(
-            SimpleCard("Hello World", speech_text))
+        handler_input.response_builder.speak(response).ask(response).set_card(
+            SimpleCard("Check Ferry", response))
         return handler_input.response_builder.response
 
 
@@ -70,10 +93,10 @@ class CancelAndStopIntentHandler(AbstractRequestHandler):
 
     def handle(self, handler_input):
         # type: (HandlerInput) -> Response
-        speech_text = "Goodbye!"
+        response = "Goodbye!"
 
-        handler_input.response_builder.speak(speech_text).set_card(
-            SimpleCard("Hello World", speech_text)).set_should_end_session(True)
+        handler_input.response_builder.speak(response).set_card(
+            SimpleCard("Hello World", response)).set_should_end_session(True)
         return handler_input.response_builder.response
 
 
@@ -88,7 +111,7 @@ class AllExceptionHandler(AbstractExceptionHandler):
         # Log the exception in CloudWatch Logs
         print(exception)
 
-        speech = "Sorry, I didn't get it. Can you please say it again!!"
+        speech = "Sorry, I didn't get that"
         handler_input.response_builder.speak(speech).ask(speech)
         return handler_input.response_builder.response
 
@@ -96,7 +119,7 @@ class AllExceptionHandler(AbstractExceptionHandler):
 sb = SkillBuilder()
 
 sb.add_request_handler(LaunchRequestHandler())
-sb.add_request_handler(HelloWorldIntentHandler())
+sb.add_request_handler(CheckFerryIntentHandler())
 sb.add_request_handler(HelpIntentHandler())
 sb.add_request_handler(CancelAndStopIntentHandler())
 sb.add_request_handler(SessionEndedRequestHandler())
